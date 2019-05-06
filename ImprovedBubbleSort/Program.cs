b301fbf92346ca561ac266ac12810f905952dcfe
Marisa Wadsworth <10002444@student.toiohomai.ac.nz>
2019-05-06 16:25:02 +1200

    Added timer

--- ImprovedBubbleSort/Program.cs
+++ ImprovedBubbleSort/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,11 @@ namespace The_Bubble_Sort_Algorithm
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\nImproved Bubble Sort");
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
             int[] nums = new int[1000];
             Random rand = new Random();
             for (int i = 0; i < nums.Length; i++)
@@ -17,28 +23,29 @@ namespace The_Bubble_Sort_Algorithm
                 nums[i] = rand.Next(1000);
             };
 
-            Console.WriteLine("");
-            Console.WriteLine("Random Numbers:");
+            Console.WriteLine("\nRandom Numbers:");
 
             foreach (int x in nums)
                 Console.Write(x + " ");
 
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Sorted Numbers:");
+            Console.WriteLine("\n\nSorted Numbers:");
 
             BubbleSort(nums);
             Display(nums);
 
+            stopwatch.Stop();
+            Console.WriteLine($"\n\nProcessing time: {stopwatch.ElapsedMilliseconds} milliseconds\n");
+
         }
-        /*Basic bubble sort algorithm*/
         static int[] BubbleSort(int[] arr)
         {
+            bool swap = true;
             /*External loop makes sure we check all of array*/
             for (int i = 0; i < arr.Length - 1; i++)
             {
-                /*Internal loop goes through the elements and checks each one*/
-                for (int j = 0; j < arr.Length - 1; j++)
+                swap = false;
+                /*Internal loop goes through the elements and checks each one and subtracts i as the largest element in the array is at the end now*/
+                for (int j = 0; j < arr.Length - 1 - i; j++)
                 {
                     /*If the current element is larger than the next element, swap them*/
                     if (arr[j] > arr[j + 1])
@@ -46,9 +53,15 @@ namespace The_Bubble_Sort_Algorithm
                         int temp = arr[j + 1];
                         arr[j + 1] = arr[j];
                         arr[j] = temp;
+                        /*check if any swaps have been made for data thats in near order*/
+                        swap = true;
                     }
                 }
+                /*If no swaps are made break*/
+                if (!swap)
+                    break;
             }
+            /*Retuern the sorted array*/
             return arr;
         }
 
--- StandardBubbleSort/Program.cs
+++ StandardBubbleSort/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
 
 namespace The_Bubble_Sort_Algorithm
 {
@@ -10,6 +13,11 @@ namespace The_Bubble_Sort_Algorithm
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\nStandard Bubble Sort");
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
             int[] nums = new int[1000];
             Random rand = new Random();
             for (int i = 0; i < nums.Length; i++)
@@ -17,29 +25,28 @@ namespace The_Bubble_Sort_Algorithm
                 nums[i] = rand.Next(1000);
             };
 
-            Console.WriteLine("");
-            Console.WriteLine("Random Numbers:");
+            Console.WriteLine("\nRandom Numbers:");
 
             foreach (int x in nums)
                 Console.Write(x + " ");
 
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Sorted Numbers:");
+            Console.WriteLine("\n\nSorted Numbers:");
 
             BubbleSort(nums);
             Display(nums);
 
+            stopwatch.Stop();
+            Console.WriteLine($"\n\nProcessing time: {stopwatch.ElapsedMilliseconds} milliseconds\n");
+
         }
+        /*Basic bubble sort algorithm*/
         static int[] BubbleSort(int[] arr)
         {
-            bool swap = true;
             /*External loop makes sure we check all of array*/
             for (int i = 0; i < arr.Length - 1; i++)
             {
-                swap = false;
-                /*Internal loop goes through the elements and checks each one and subtracts i as the largest element in the array is at the end now*/
-                for (int j = 0; j < arr.Length - 1 - i; j++)
+                /*Internal loop goes through the elements and checks each one*/
+                for (int j = 0; j < arr.Length - 1; j++)
                 {
                     /*If the current element is larger than the next element, swap them*/
                     if (arr[j] > arr[j + 1])
@@ -47,15 +54,9 @@ namespace The_Bubble_Sort_Algorithm
                         int temp = arr[j + 1];
                         arr[j + 1] = arr[j];
                         arr[j] = temp;
-                        /*check if any swaps have been made for data thats in near order*/
-                        swap = true;
                     }
                 }
-                /*If no swaps are made break*/
-                if (!swap)
-                    break;
             }
-            /*Retuern the sorted array*/
             return arr;
         }
 
